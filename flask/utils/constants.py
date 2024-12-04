b4b27ae43415b065dec6b4be1f537755c27cfc97
Deval1509 <109296657+Deval1509@users.noreply.github.com>
2024-12-04 22:57:40 +0000

    App.py changes

--- flask/utils/constants.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-BASE_URL = "https://financialmodelingprep.com/api/v3"
-API_KEY = os.getenv("API_KEY", "FUBR1JfW34uaqrFUTKQ82EpOH9z8JrvF")
